6b8e7cb5605523f7ca6bf0b2dc4c38dbeaa588c5
Marek Parucha <mparucha@stud.hs-heilbronn.de>
2017-07-17 23:37:10 +0200

    Intitialized Project

---
+++ desktop/src/de/marekparucha/newtowerdefense/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package de.marekparucha.newtowerdefense.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import de.marekparucha.newtowerdefense.NewTowerDefense;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new NewTowerDefense(), config);
+	}
+}
